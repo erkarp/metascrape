5f6f42ed09aa40032326dd04db4dc4b2591554e9
Emily <emilyrkarp@gmail.com>
2016-02-01 10:34:24 -0500

    init: pseudocode

---
+++ script.js
@@ -0,0 +1,32 @@
+/*
+1.
+getFn - path return html
+
+2.
+addPage - arr, obj noreturn arr.push(obj)
+
+3.
+find : links w same url, return arr
+
+4.
+getTitle(html) return title.inner
+
+5.
+metalist - []
+
+6.
+byId get item by id return item
+
+7.
+meta - path, html, rec=true
+	if !byId(html)
+		addPage(metalist, {path:path,title:getTitle()})
+	if rec
+		arr = find()
+		arr.foreach(g - this link, false)
+
+8.
+FROM_USER - path
+  html = getFn(path)
+  meta(path, html)
+*/
---
+++ test/test.js
@@ -0,0 +1,35 @@
+/*
+emptyarr = []
+mockpage = file
+mockpath = filename
+mocknode = {..}
+mockmeta = [{..}..]
+mocklinks = number
+mocktitle = string
+
+1.
+getFnf(mockpath) == mockpage
+
+2.
+addFn(emptyarr, mocknode)
+emptyarr.length == 1
+
+3.
+page = html
+linkArr = [link...]
+find(page) == linkArr
+
+4.
+getTitle(mockpage) == mocktitle
+
+5.
+metadata.alltrue(fn - typeof i == obj)
+
+6.
+byId(mockmeta id) == mocknode.id
+
+7, 8.
+FROM_USER - mockpath
+  metalist == mockmeta
+
+*/
