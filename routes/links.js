32d68ac3b30344eb05eb3fe3c8f02138f64cce90
Emily <emilyrkarp@gmail.com>
2016-05-19 10:36:30 -0400

    dropping hrefs with unfamiliar hostnames

--- routes/links.js
+++ routes/links.js
@@ -4,9 +4,12 @@ var request = require("request");
 var cheerio = require("cheerio");
 var scrape = require('../tasks/script.js');
 var router = express.Router();
+var parse = require('url-parse');
 
 router.post('/', function(req, res, next){
-    var text = req.body.website;
+    var text = req.body.website,
+        parts = text.split('/'),
+        url = parse(text);
 
     request(text, function (error, response, body) {
       if (!error) {
@@ -17,8 +20,17 @@ router.post('/', function(req, res, next){
         $('a').each(function(i, elem) {
           var href = $(this).attr('href');
 
+          if (href.length <= 1 || href.indexOf('#') === 0 || href.includes('mailto')) {
+            return;
+          }
+
+          var hrefHostname = parse(href).hostname;
+          if (hrefHostname && hrefHostname !== url.hostname) {
+            return;
+          }
+
           if (href[0] === '/') {
-            href = href.slice(1);
+            href = parts[0] + '//' + parts[2] + href;
           }
 
           if (href.length > 0) {
@@ -28,17 +40,13 @@ router.post('/', function(req, res, next){
         });
 
         links = links.sort().filter(function(elem, i, arr) {
-          if (arr[i] === arr[i-1]) {
-            return false;
-          } else {
-            return true;
-          }
+          return arr[i] === arr[i-1] ? false : true;
         });
 
-       res.render('links', {
-         title: title,
-         links: links
-       });
+        res.render('links', {
+          title: title,
+          links: links
+        });
 
         console.log("It’s " + title + " degrees Fahrenheit.");
       } else {
