aa12bd71399005078937f421f76d4566600fe0b9
Emily <emilyrkarp@gmail.com>
2016-05-19 07:13:30 -0400

    showing a list of non-repeating hrefs

--- routes/links.js
+++ routes/links.js
@@ -6,20 +6,41 @@ var scrape = require('../tasks/script.js');
 var router = express.Router();
 
 router.post('/', function(req, res, next){
-    var text = req.body.website,
-        text = 'http://emilykarp.com/';
+    var text = req.body.website;
 
     request(text, function (error, response, body) {
       if (!error) {
         var $ = cheerio.load(body),
-          temperature = $("h2");
+          title = $('title').html(),
+          links = [];
 
-         res.render('links', {
-           title: temperature,
-           links: text
-         });
+        $('a').each(function(i, elem) {
+          var href = $(this).attr('href');
 
-        console.log("It’s " + temperature + " degrees Fahrenheit.");
+          if (href[0] === '/') {
+            href = href.slice(1);
+          }
+
+          if (href.length > 0) {
+            links.push(href);
+          }
+
+        });
+
+        links = links.sort().filter(function(elem, i, arr) {
+          if (arr[i] === arr[i-1]) {
+            return false;
+          } else {
+            return true;
+          }
+        });
+
+       res.render('links', {
+         title: title,
+         links: links
+       });
+
+        console.log("It’s " + title + " degrees Fahrenheit.");
       } else {
         console.log("We’ve encountered an error: " + error);
       }
